676ba0e302b2d0192da9345d0e111917459ba95a
computer-group <computer-group>
2021-11-20 11:53:31 -0800

    first commit

---
+++ example.sh
@@ -0,0 +1,5 @@
+"d:\computer-group\.portables\git\bin\git" --version;
+#git commit -m "first commit"
+#git remote add origin git@github.com:e-ahmedwaleed/cnn-eia.git
+#git push -u origin master
+cmd /k command;
