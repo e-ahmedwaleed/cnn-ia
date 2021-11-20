1c7839d99a3cdc52fa85a70300cc3192c51c8e7b
computer-group <computer-group>
2021-11-20 12:11:03 -0800

    clean start

--- example.sh
+++
@@ -1,5 +0,0 @@
-"d:\computer-group\.portables\git\bin\git" --version;
-#git commit -m "first commit"
-#git remote add origin git@github.com:e-ahmedwaleed/cnn-eia.git
-#git push -u origin master
-cmd /k command;
