a62904aea5d21b8abe673e18ea5146b92f7d5a7b
Ahmed Waleed <ahmedwaleed@csed22.com>
2021-11-27 06:48:47 +0200

    fixed missing import modules helpful: https://stackoverflow.com/questions/2349991/how-to-import-other-python-files

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+import os
 import sys
+
+file_dir = os.path.dirname(__file__)
+sys.path.append(file_dir)
 
 from PyQt5 import QtWidgets
 from parameters_extractor import ParametersExtractor, ParametersExtractorGUI
