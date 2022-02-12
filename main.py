454d4d34a6573b223239593d180526af13ad6ebe
Ahmed Waleed <ahmedwaleed@csed22.com>
2022-02-12 21:33:35 +0200

    run_python test

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-print("\nAvailable Options:")
+import os.path
+import subprocess
+
+print("\nAvailable options:")
 print("_________________\n")
 
 print("(1) cnn-model parameters extractor")
 print("(2) interstellar correctness tests")
 
-print("\n(0) Exit\n")
+print("\n(0) exit\n")
 
 choice = 0
 
@@ -18,7 +21,22 @@ while True:
     except IndexError:
         continue
 
+
+def run_python(_py_file, _stdout=False, _stderr=False):
+    _cmd = "python "
+    if os.path.exists("./venv/Scripts/python.exe"):
+        _cmd = "./venv/Scripts/python.exe "
+
+    _cmd += _py_file
+    _proc = subprocess.run(_cmd.split(), capture_output=True)
+    if _stdout:
+        print(_proc.stdout.decode('ascii'))
+    if _stderr:
+        print(_proc.stderr.decode('ascii'))
+    return _proc
+
+
 if choice == 1:
-    print("cnn-model parameters extractor...")
+    print(run_python("./phase-1-extractor/main.py").returncode == 0)
 elif choice == 2:
     print("interstellar correctness tests...")
