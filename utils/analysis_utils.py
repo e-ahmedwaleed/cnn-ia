3ae541da53c624d9196b25ab3518830ee1130614
Ahmed Waleed <ahmedwaleed@csed22.com>
2022-03-30 04:21:54 +0200

    empty analysis
    file handling:
    https://www.geeksforgeeks.org/file-handling-python
    
    sorting a set:
    https://stackoverflow.com/questions/17457793/sorting-a-set-of-values

--- utils/analysis_utils.py
+++ utils/analysis_utils.py
@@ -1,5 +1,5 @@
 import os
-from utils.python_utils import name_of, list_files
+from utils.python_utils import name_of, list_files, create_dir, TIMEOUT
 
 
 def file_name(_path):
@@ -32,8 +32,18 @@ def list_outputs(_arch):
     return o_files
 
 
-def identify_subdir(_parent_dir, _file):
-    return _file[_file.rfind(_parent_dir) + len(_parent_dir):_file.rfind('/')]
+def identify_output_label(_parent_dir, _file):
+    subdir = _file[_file.rfind(_parent_dir) + len(_parent_dir):_file.rfind('/')]
+
+    if subdir:
+        if "exceptions" in subdir:
+            label = "exception {" + subdir[subdir.rfind("/") + 1:] + "}"
+        else:
+            label = "timeout {" + str(TIMEOUT) + "}"
+    else:
+        label = "ok {" + str(TIMEOUT) + "}"
+
+    return label
 
 
 layers_set = set()
@@ -42,11 +52,35 @@ schedules_set = set()
 outputs_set = set()
 
 
+def save_output_set(_dir, _set):
+    create_dir(_dir)
+
+    for item in _set:
+        file = open(_dir + str(item) + '.csv', 'w')
+        file.close()
+    file = open(_dir + '.ranking', 'w')
+    file.close()
+
+
 def save_outputs(_optimizer_type, _outputs):
-    print(len(layers_set))
-    print(len(archs_set))
-    print(len(schedules_set))
-    print(len(outputs_set))
+    output_dir = "./interstellar-output-analysis/"
+    create_dir(output_dir)
+    optimizer_dir = output_dir + _optimizer_type + "/"
+    create_dir(optimizer_dir)
+
+    layers_dir = optimizer_dir + "layer/"
+    save_output_set(layers_dir, layers_set)
+    archs_dir = optimizer_dir + "arch/"
+    save_output_set(archs_dir, archs_set)
+
+    if _optimizer_type != "dataflow":
+        schedules_dir = optimizer_dir + "schedule/"
+        save_output_set(schedules_dir, schedules_set)
+
+    file = open(optimizer_dir + 'labels.txt', 'w')
+    for label in sorted(outputs_set):
+        file.write(str(label) + '\n')
+    file.close()
 
 
 def analyze_interstellar_samples(_optimizer_type):
@@ -60,8 +94,8 @@ def analyze_interstellar_samples(_optimizer_type):
         layer_dir = optimizer_dir + layer + "/"
         if _optimizer_type == "dataflow":
             for output in list_outputs(layer_dir):
-                outputs[layer][file_name(output)] = identify_subdir(layer, output)
-                outputs_set.add(identify_subdir(layer, output))
+                outputs[layer][file_name(output)] = identify_output_label(layer, output)
+                outputs_set.add(identify_output_label(layer, output))
                 archs_set.add(file_name(output))
         else:
             archs = list_dirs(layer_dir)
@@ -69,8 +103,8 @@ def analyze_interstellar_samples(_optimizer_type):
                 outputs[layer][arch] = {}
                 arch_dir = layer_dir + arch + "/"
                 for output in list_outputs(arch_dir):
-                    outputs[layer][arch][file_name(output)] = identify_subdir(arch, output)
-                    outputs_set.add(identify_subdir(arch, output))
+                    outputs[layer][arch][file_name(output)] = identify_output_label(arch, output)
+                    outputs_set.add(identify_output_label(arch, output))
                     schedules_set.add(file_name(output))
                 archs_set.add(arch)
         layers_set.add(layer)
